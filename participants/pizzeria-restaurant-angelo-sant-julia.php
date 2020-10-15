495bffeb369ce3df65ad6e6293e3837c3ebfb5f4
JaraBlocD <jara@blocd.com>
2020-10-15 16:26:38 +0200

    Create pizzeria-restaurant-angelo-sant-julia.php
    Pizzeria rest angelo sant julia CA

---
+++ participants/pizzeria-restaurant-angelo-sant-julia.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="ca" class="ficha-restaurante">
+  <head>
+    <meta charset="utf-8">
+    <title>1940 Restaurant</title>
+    <?php include dirname(__FILE__).'/../template-parts/head.html'; ?>
+  </head>
+  <body>
+    <div class="hamburguer">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+
+   
+    <div data-barba="wrapper">
+    	<header>
+        <?php include dirname(__FILE__).'/../template-parts/header.html'; ?>
+        <!-- START Menú > cambio de idiomas -->
+        <nav class="menu-lang">
+          <li><a href="es/participantes/pizzeria-restaurant-angelo-sant-julia.php">ES</a></li>
+          <li><a href="fr/participants/pizzeria-restaurant-angelo-sant-julia.php">FR</a></li>
+          <li><a href="participants/pizzeria-restaurant-angelo-sant-julia.php">CA</a></li>
+        </nav>
+        <!-- END Menú > cambio de idiomas -->
+    	</header>
+      <div class="restaurant" data-barba="container" data-barba-namespace="restaurant">
+        <a class="circletopo" href="http://localhost/andorra">
+          <div class="circle titulo">
+            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en família</span></h1>
+          </div>
+        </a>
+        <div class="barra-left">
+          <div class="container">
+            <div class="aligner"></div>
+            <!-- START BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <h2>Pizzeria Restaurant<br>Angelo Sant Julià</h2>
+            </div>
+            <!-- FIN BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <div class="info-menu">
+                <!-- START MENU > TIPO MENU 1 -->
+                <div class="menu">
+                  <h4>Menú gastronòmic</h4>
+                  <ul>
+                    <li>Croqueta de botifarra negra<br>amb poma i formatge de cabra<br>acompanyada d’una Inedit Damm</li>
+                    —<br>
+                    <li>Sopa minestrone de verdures de temporada,<br>brou i pasta</li>
+                    —<br>
+                    <li>Carpaccio de bacallà<br>sobre fons de tomàquet natural,<br>escuma de pebrot del piquillo i pa pintu</li>
+                    —<br>
+                    <li>Raviolis farcits de carn de vedella i verdures<br>amb salsa de tòfona i encenalls de parmesà</li>
+                    —<br>
+                    <li>Presa de porc de raça bísara<br>amb emulsió de xirivia i chimichurri de festucs</li>
+                    -<br>
+                    <li>Crema sabaiona de pera<br>i ametlles gratinades</li>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>30 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 1 -->
+
+                <!-- START MENU > TIPO MENU 2 -->
+                <div class="menu">
+                  <h4>Menú junior</h4>
+                  <ul>
+                    <li>Croqueta de pernil<br>de porc de raça bísara</li>
+                    —<br>
+                    <li>Crema de carbassa amb trossets de castanya<br>a la sal i encenalls de pernil de porc de raça bísara</li>
+                    —<br>
+                    <li>Ravioli farcit de carn amb sala de tomàquet,<br>formatge parmesà i alfàbrega</li>
+                    —<br>
+                    <li>Crep amb Nutella</li>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>15 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 2 -->
+            </div>
+          </div>
+
+          <!-- START LEGAL + DIRECCIÓN -->
+          <div class="info-restaurant">
+            <div class="direccion">
+              <p>IGI inclòs • Begudes no incloses</p>
+            </div>
+             <div class="boton">
+              <a href="https://www.pizzeriaangelo.restaurant/" target="_blank">Fes la teva <br>reserva</a>
+              <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
+            </div>
+            <div class="direccion">
+              <p>Av. Rocafort, 27 · Sant Julià de Lòria</p>
+              <p>(+376) 742 274</p>
+              <p> De dilluns a diumenge, de 13 h a 15.30 h</p>
+              <p>i de 20 h a 23 h</p>
+              <p>administracio@grupkanalla.com</p>
+              <br>
+              <p><a href="https://www.pizzeriaangelo.restaurant/">www.pizzeriaangelo.restaurant</a></p>
+              <p><a href="#">@angelo.santjulia</a></p>
+            </div>
+          </div>
+          <!-- END LEGAL + DIRECCIÓN -->
+        </div>
+        </div>
+
+        <!-- START IMÁGENES LINKS -->
+        <div class="gallery">
+          <div class="slide" style="background-image: url('img/participants/andorra-park-hotel-restaurant-es/01.jpg')"><div class="tipomenu">Menú Junior</div></div>
+          <div class="slide">your 2</div>
+          <div class="slide">your 3</div>
+        </div>
+        <!-- END IMÁGENES LINKS -->
+
+      </div>
+    </div>
+
+    <?php include dirname(__FILE__).'/../template-parts/footer.html'; ?>
+    
+  </body>
+</html>
