bfd5ab4f4ca4bbf2230f80a005ff37e52c68fa1a
JaraBlocD <jara@blocd.com>
2020-10-15 16:42:56 +0200

    Create restaurant-la-borda-de-lavi.php
    Rest la borda de l'avi CA

---
+++ participants/restaurant-la-borda-de-lavi.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="ca" class="ficha-restaurante">
+  <head>
+    <meta charset="utf-8">
+    <title>1940 Restaurant</title>
+    <?php include dirname(__FILE__).'/../template-parts/head.html'; ?>
+  </head>
+  <body>
+    <div class="hamburguer">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+
+   
+    <div data-barba="wrapper">
+    	<header>
+        <?php include dirname(__FILE__).'/../template-parts/header.html'; ?>
+        <!-- START Menú > cambio de idiomas -->
+        <nav class="menu-lang">
+          <li><a href="es/participantes/restaurant-la-borda-de-lavi.php">ES</a></li>
+          <li><a href="fr/participants/restaurant-la-borda-de-lavi.php">FR</a></li>
+          <li><a href="participants/restaurant-la-borda-de-lavi.php">CA</a></li>
+        </nav>
+        <!-- END Menú > cambio de idiomas -->
+    	</header>
+      <div class="restaurant" data-barba="container" data-barba-namespace="restaurant">
+        <a class="circletopo" href="http://localhost/andorra">
+          <div class="circle titulo">
+            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en família</span></h1>
+          </div>
+        </a>
+        <div class="barra-left">
+          <div class="container">
+            <div class="aligner"></div>
+            <!-- START BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <h2>Restaurant La borda de l'avi</h2>
+            </div>
+            <!-- FIN BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <div class="info-menu">
+                <!-- START MENU > TIPO MENU 1 -->
+                <div class="menu">
+                  <h4>Menú tradicional</h4>
+                  <ul>
+                    <li>Aperitiu Andorra a Taula<br>acompanyat d’una Inedit Damm</li>
+                    —<br>
+                    <li>Amanida tèbia de bacallà<br>brou i pasta</li>
+                    —<br>
+                    <li>Carpaccio de bacallà<br>amb pebrots vermells escalivats<br>i vinagreta de miso<br> o <br>Canelons tradicionals gratinats<br>amb beixamel de ceps</li>
+                    —<br>
+                    <li>Calamars a la planxa<br>damunt un llit de favetes saltades<br> o <br>Entrecot de vedella del país a la brasa<br>amb patata al caliu, tomàquet a la provençal<br>i pebrots del Padrón
+                    —<br>
+                    <li>Flam de l’àvia de castanyes<br> o <br>Pastís Tatin de pera amb gelat de nata</li>
+                    -<br>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>38 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 1 -->
+
+                <!-- START MENU > TIPO MENU 2 -->
+                <div class="menu">
+                  <h4>Menú junior</h4>
+                  <ul>
+                    <li>Aperitiu Andorra a Taula</li>
+                    —<br>
+                    <li>Crema de carbassa<br>amb encenalls de pernil ibèric</li>
+                    —<br>
+                    <li>RMinientrecot del país a la brasa<br>amb patates fregides</li>
+                    —<br>
+                    <li>Coulant de xocolata<br>amb sorbet de llimona</li>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>20 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 2 -->
+            </div>
+          </div>
+
+          <!-- START LEGAL + DIRECCIÓN -->
+          <div class="info-restaurant">
+            <div class="direccion">
+              <p>IGI inclòs • Begudes no incloses</p>
+            </div>
+             <div class="boton">
+              <a href="https://www.restaurantelabordadelavi.com/" target="_blank">Fes la teva <br>reserva</a>
+              <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
+            </div>
+            <div class="direccion">
+              <p>Av. del Ravell, 38 · La Massana</p>
+              <p>(+376) 835 154</p>
+              <p> De dilluns a diumenge, de 13 h a 15.30 h</p>
+              <p>i de 20 h a 22.30 h</p>
+              <p>bordaavi@andorra.ad</p>
+              <br>
+              <p><a href="https://www.restaurantelabordadelavi.com/">www.restaurantelabordadelavi.com</a></p>
+              <p><a href="#">@bordadelavi</a></p>
+            </div>
+          </div>
+          <!-- END LEGAL + DIRECCIÓN -->
+        </div>
+        </div>
+
+        <!-- START IMÁGENES LINKS -->
+        <div class="gallery">
+          <div class="slide" style="background-image: url('img/participants/andorra-park-hotel-restaurant-es/01.jpg')"><div class="tipomenu">Menú Junior</div></div>
+          <div class="slide">your 2</div>
+          <div class="slide">your 3</div>
+        </div>
+        <!-- END IMÁGENES LINKS -->
+
+      </div>
+    </div>
+
+    <?php include dirname(__FILE__).'/../template-parts/footer.html'; ?>
+    
+  </body>
+</html>
