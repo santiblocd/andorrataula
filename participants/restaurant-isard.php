acfebd42f1003c38aa877833fd0d21017e10650f
JaraBlocD <jara@blocd.com>
2020-10-16 11:29:04 +0200

    isard CA y corección coma

--- participants/restaurant-isard.php
+++ participants/restaurant-isard.php
@@ -2,7 +2,7 @@
 <html lang="ca" class="ficha-restaurante">
   <head>
     <meta charset="utf-8">
-    <title>Hotel Coma</title>
+    <title>Restaurant Isard</title>
     <?php include dirname(__FILE__).'/../template-parts/head.html'; ?>
   </head>
   <body>
@@ -18,9 +18,9 @@
         <?php include dirname(__FILE__).'/../template-parts/header.html'; ?>
         <!-- START Menú > cambio de idiomas -->
         <nav class="menu-lang">
-          <li><a href="es/participantes/hotel-coma.php">ES</a></li>
-          <li><a href="fr/participants/hotel-coma.php">FR</a></li>
-          <li><a href="participants/hotel-coma.php">CA</a></li>
+          <li><a href="es/participantes/restaurant-isard.php">ES</a></li>
+          <li><a href="fr/participants/restaurant-isard.php">FR</a></li>
+          <li><a href="participants/restaurant-isard.php">CA</a></li>
         </nav>
         <!-- END Menú > cambio de idiomas -->
     	</header>
@@ -35,23 +35,26 @@
             <div class="aligner"></div>
             <!-- START BLOQUE RESTAURANTE > TITULO -->
             <div class="info-restaurant">
-              <h2>Hotel Coma</h2>
+              <h2>Restaurant Isard</h2>
             </div>
             <!-- FIN BLOQUE RESTAURANTE > TITULO -->
             <div class="info-restaurant">
               <div class="info-menu">
                 <!-- START MENU > TIPO MENU 1 -->
                 <div class="menu">
-                  <h4>Menú tradicional</h4>
+                  <h4>Menú gastronòmic</h4>
                   <ul>
                     <li>Aperitiu Andorra a Taula<br>acompanyat d'una Inedit Damm</li>
                     —<br>
-                    <li>Trinxat amb rosta de cansalada<br>viada i bitxo<br> o <br>Amanida de picantó escabetxat</li>
+                    <li>Porros i carbassa confitats<br>amb dashi i caviar de truita de riu</li>
                     —<br>
-                    <li>Cua de bou amb bolets<br> o <br>Bacallà confitat amb farcellets de col</li>
+                    <li>Ceps amb cansalada i llardons</li>
                     —<br>
-                    <li>Figues confitades al whisky<br>amb gelat<br> o <br>Tarta fina de poma<br>amb gelat de vainilla i pinyons</li>
+                    <li>Calamars farcits de botifarra<br>sobre crema de cranc de riu</li>
                     —<br>
+                    <li>Cua de bou desossada amb ratassia<br>i trompetes de la mort</li>
+                    -<br>
+                    <li>Pastís Tatin de caqui<br>amb gelat de marialluïsa</li>
                   </ul>
                 </div>
                 <div class="preu">
@@ -66,15 +69,15 @@
                   <ul>
                     <li>Aperitiu Andorra a Taula</li>
                     —<br>
-                    <li>Croquetons de jarret de vedella<br>amb tomàquet amanit</li>
+                    <li>Macarrons gratinats</li>
                     —<br>
-                    <li>Costelles de xai<br>amb patates fregides casolanes</li>
+                    <li>Llibrets de llom<br>amb formatge i pernil dolç</li>
                     —<br>
-                    <li>Gelats artesans</li>
+                    <li>GBrownie de xocolata<br>amb gelat de vainilla</li>
                   </ul>
                 </div>
                 <div class="preu">
-                  <p>16 €</p>
+                  <p>15 €</p>
                   <span>per persona</span>
                 </div>
                 <!-- FIN MENU > TIPO MENU 2 -->
@@ -87,18 +90,18 @@
               <p>IGI inclòs • Begudes no incloses</p>
             </div>
              <div class="boton">
-              <a href="https://www.hotelcoma.com" target="_blank">Fes la teva <br>reserva</a>
+              <a href="mailto:reserves@hotelisard.com" target="_blank">Fes la teva <br>reserva</a>
               <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
             </div>
             <div class="direccion">
-              <p>Camp de La Tanada, s/n · Ordino</p>
-              <p>(+376) 736 100</p>
-              <p> De dilluns a diumenge, de 13 h a 15 h</p>
-              <p>i de 20 h a 22 h</p>
-              <p>hotelcoma@hotelcoma.com</p>
+              <p>Av. Meritxell, 34 · Andorra la Vella</p>
+              <p>(+376) 876 800</p>
+              <p> De dilluns a diumenge, de 13 h a 15.30 h</p>
+              <p>i de 20 h a 22.30 h</p>
+              <p>reserves@hotelisard.com</p>
               <br>
-              <p><a href="https://www.hotelcoma.com/">www.hotelcoma.com</a></p>
-              <p><a href="#">@hotelcoma</a></p>
+              <p><a href="https://www.hotelisard.com/">www.hotelisard.com</a></p>
+              <p><a href="#">@isardandorra</a></p>
             </div>
           </div>
           <!-- END LEGAL + DIRECCIÓN -->
