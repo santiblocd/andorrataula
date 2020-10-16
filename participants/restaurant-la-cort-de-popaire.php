6ae2f9d3fba3fd28f02f3fb0260dc569e1dfcbee
JaraBlocD <jara@blocd.com>
2020-10-16 10:35:14 +0200

    Create restaurant-la-cort-de-popaire.php
    creado CA

---
+++ participants/restaurant-la-cort-de-popaire.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="ca" class="ficha-restaurante">
+  <head>
+    <meta charset="utf-8">
+    <title>Restaurant La Cort de Popaire</title>
+    <?php include dirname(__FILE__).'/../template-parts/head.html'; ?>
+  </head>
+  <body>
+    <div class="hamburguer">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+
+   
+    <div data-barba="wrapper">
+    	<header>
+        <?php include dirname(__FILE__).'/../template-parts/header.html'; ?>
+        <!-- START Menú > cambio de idiomas -->
+        <nav class="menu-lang">
+          <li><a href="es/participantes/restaurant-la-cort-de-popaire.php">ES</a></li>
+          <li><a href="fr/participants/restaurant-la-cort-de-popaire.php">FR</a></li>
+          <li><a href="participants/restaurant-la-cort-de-popaire.php">CA</a></li>
+        </nav>
+        <!-- END Menú > cambio de idiomas -->
+    	</header>
+      <div class="restaurant" data-barba="container" data-barba-namespace="restaurant">
+        <a class="circletopo" href="http://localhost/andorra">
+          <div class="circle titulo">
+            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en família</span></h1>
+          </div>
+        </a>
+        <div class="barra-left">
+          <div class="container">
+            <div class="aligner"></div>
+            <!-- START BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <h2>Restaurant La Cort de Popaire</h2>
+            </div>
+            <!-- FIN BLOQUE RESTAURANTE > TITULO -->
+            <div class="info-restaurant">
+              <div class="info-menu">
+                <!-- START MENU > TIPO MENU 1 -->
+                <div class="menu">
+                  <h4>Menú gastronòmic</h4>
+                  <ul>
+                    <li>Aperitiu Andorra a Taula<br>acompanyat d’una Inedit Damm</li>
+                    —<br>
+                    <li>Cheesecake de truita fumada<br>amb anet i mostassa</li>
+                    —<br>
+                    <li>Gyozes andorranes de donja,<br>botifarra negra i trinxat</li>
+                    —<br>
+                    <li>Salmó amb fonoll confitat<br>i reducció de ratassia de la Carmeta</li>
+                    -<br>
+                    <li>Magret d’ànec a la brasa<br>amb de patates fregides de codony</li>
+                    -<br>
+                    <li>Brownie de xocolata i figues<br>amb coulis de macarulla</li>
+                    -<br>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>35 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 1 -->
+
+                <!-- START MENU > TIPO MENU 2 -->
+                <div class="menu">
+                  <h4>Menú junior</h4>
+                  <ul>
+                    <li>Aperitiu Andorra a Taula</li>
+                    —<br>
+                    <li>Croquetes de ceps o pollastre<br>amb amanida variada</li>
+                    —<br>
+                    <li>Botifarra a la brasa<br>amb patates fregides i verdures</li>
+                    —<br>
+                    <li>Brownie de xocolata<br>amb gelat de vainilla</li>
+                  </ul>
+                </div>
+                <div class="preu">
+                  <p>15 €</p>
+                  <span>per persona</span>
+                </div>
+                <!-- FIN MENU > TIPO MENU 2 -->
+            </div>
+          </div>
+
+          <!-- START LEGAL + DIRECCIÓN -->
+          <div class="info-restaurant">
+            <div class="direccion">
+              <p>IGI inclòs • Begudes no incloses</p>
+            </div>
+             <div class="boton">
+              <a href="mailto:lacortdepopaire@andorra.ad" target="_blank">Fes la teva <br>reserva</a>
+              <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
+            </div>
+            <div class="direccion">
+              <p>Plaça del poble · Soldeu</p>
+              <p>(+376) 851 211</p>
+              <p> De dijous a diumenge, de 13 h a 15 h</p>
+              <p>i de 20 h a 22 h</p>
+              <p>dilluns, dimarts i dimecres, tancat</p>
+              <p>lacortdepopaire@andorra.ad</p>
+              <br>
+              <p><a href="#">@lacortdepopaire</a></p>
+            </div>
+          </div>
+          <!-- END LEGAL + DIRECCIÓN -->
+        </div>
+        </div>
+
+        <!-- START IMÁGENES LINKS -->
+        <div class="gallery">
+          <div class="slide" style="background-image: url('img/participants/andorra-park-hotel-restaurant-es/01.jpg')"><div class="tipomenu">Menú Junior</div></div>
+          <div class="slide">your 2</div>
+          <div class="slide">your 3</div>
+        </div>
+        <!-- END IMÁGENES LINKS -->
+
+      </div>
+    </div>
+
+    <?php include dirname(__FILE__).'/../template-parts/footer.html'; ?>
+    
+  </body>
+</html>
