ed843bcf62a87c49cef503be13f0b9b0ddb81510
JaraBlocD <jara@blocd.com>
2020-10-19 16:44:47 +0200

    Update hotel-coma.php

--- es/participantes/hotel-coma.php
+++ es/participantes/hotel-coma.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ca" class="ficha-restaurante">
+<html lang="es" class="ficha-restaurante">
   <head>
     <meta charset="utf-8">
     <title>Hotel Coma</title>
@@ -27,7 +27,7 @@
       <div class="restaurant" data-barba="container" data-barba-namespace="restaurant">
         <a class="circletopo" href="http://localhost/andorra">
           <div class="circle titulo">
-            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en família</span></h1>
+            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en familia</span></h1>
           </div>
         </a>
         <div class="barra-left">
@@ -44,38 +44,38 @@
                 <div class="menu">
                   <h4>Menú tradicional</h4>
                   <ul>
-                    <li>Aperitiu Andorra a Taula<br>acompanyat d'una Inedit Damm</li>
+                    <li>Aperitivo Andorra a Taula<br>acompañado de una Inedit Damm</li>
                     —<br>
-                    <li>Trinxat amb rosta de cansalada<br>viada i bitxo<br> o <br>Amanida de picantó escabetxat</li>
+                    <li>Trinxat con torrezno de panceta y guindilla<br> o <br>Ensalada de picantón escabechado</li>
                     —<br>
-                    <li>Cua de bou amb bolets<br> o <br>Bacallà confitat amb farcellets de col</li>
+                    <li>Rabo de buey con setas<br> o <br>Bacalao confitado con rollitos de col</li>
                     —<br>
-                    <li>Figues confitades al whisky<br>amb gelat<br> o <br>Tarta fina de poma<br>amb gelat de vainilla i pinyons</li>
+                    <li>Higos confitados al whisky<br>con helado<br> o <br>Tarta fina de manzana<br>con helado de vainilla y piñones</li>
                     —<br>
                   </ul>
                 </div>
                 <div class="preu">
                   <p>32 €</p>
-                  <span>per persona</span>
+                  <span>por persona</span>
                 </div>
                 <!-- FIN MENU > TIPO MENU 1 -->
 
                 <!-- START MENU > TIPO MENU 2 -->
                 <div class="menu">
-                  <h4>Menú júnior</h4>
+                  <h4>Menú junior</h4>
                   <ul>
-                    <li>Aperitiu Andorra a Taula</li>
+                    <li>Aperitivo Andorra a Taula</li>
                     —<br>
-                    <li>Croquetons de jarret de vedella<br>amb tomàquet amanit</li>
+                    <li>Croquetones de contraculata de ternera<br>con tomate aliñado</li>
                     —<br>
-                    <li>Costelles de xai<br>amb patates fregides casolanes</li>
+                    <li>Costillas de cordero<br>con patatas fritas caseras</li>
                     —<br>
-                    <li>Gelats artesans</li>
+                    <li>Helados artesanos</li>
                   </ul>
                 </div>
                 <div class="preu">
                   <p>16 €</p>
-                  <span>per persona</span>
+                  <span>por persona</span>
                 </div>
                 <!-- FIN MENU > TIPO MENU 2 -->
             </div>
@@ -84,17 +84,17 @@
           <!-- START LEGAL + DIRECCIÓN -->
           <div class="info-restaurant">
             <div class="direccion">
-              <p>IGI inclòs • Begudes no incloses</p>
+              <p>IGI incluido • Bebidas no incluidas</p>
             </div>
              <div class="boton">
-              <a href="https://www.hotelcoma.com" target="_blank">Fes la teva <br>reserva</a>
+              <a href="https://www.hotelcoma.com" target="_blank">Haz tu<br>reserva</a>
               <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
             </div>
             <div class="direccion">
               <p>Camp de La Tanada, s/n · Ordino</p>
               <p>(+376) 736 100</p>
-              <p> De dilluns a diumenge, de 13 h a 15 h</p>
-              <p>i de 20 h a 22 h</p>
+              <p>De lunes a domingo, de 13 h a 15 h</p>
+              <p>y de 20 h a 22 h</p>
               <p>hotelcoma@hotelcoma.com</p>
               <br>
               <p><a href="https://www.hotelcoma.com/">www.hotelcoma.com</a></p>
