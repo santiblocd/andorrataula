eacd388621ebacb1e32cf888da2928e569cedeb2
JaraBlocD <jara@blocd.com>
2020-10-23 11:44:19 +0200

    Update hotel-coma.php

--- fr/participants/hotel-coma.php
+++ fr/participants/hotel-coma.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ca" class="ficha-restaurante">
+<html lang="fr" class="ficha-restaurante">
   <head>
     <meta charset="utf-8">
     <title>Hotel Coma</title>
@@ -27,7 +27,7 @@
       <div class="restaurant" data-barba="container" data-barba-namespace="restaurant">
         <a class="circletopo" href="http://localhost/andorra">
           <div class="circle titulo">
-            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en família</span></h1>
+            <h1><span class="minus">Andorra</span><span><img src="./img/taula.svg"></span> <span class="mayus">en famille</span></h1>
           </div>
         </a>
         <div class="barra-left">
@@ -42,40 +42,40 @@
               <div class="info-menu">
                 <!-- START MENU > TIPO MENU 1 -->
                 <div class="menu">
-                  <h4>Menú tradicional</h4>
+                  <h4>Menu traditionnel</h4>
                   <ul>
-                    <li>Aperitiu Andorra a Taula<br>acompanyat d'una INEDIT de Damm</li>
+                    <li>Amuse-bouche Andorra a Taula<br>servi avec une bière INEDIT Damm</li>
                     —<br>
-                    <li>Trinxat amb rosta de cansalada<br>viada i bitxo<br> o <br>Amanida de picantó escabetxat</li>
+                    <li>Hachis de légumes, tranche<br>de poitrine fumée et piment rouge<br> ou <br>Salade de petit poulet mariné</li>
                     —<br>
-                    <li>Cua de bou amb bolets<br> o <br>Bacallà confitat amb farcellets de col</li>
+                    <li>Queue de bœuf aux champignons<br> ou <br>Morue confite garnie de rouleaux de choux</li>
                     —<br>
-                    <li>Figues confitades al whisky<br>amb gelat<br> o <br>Tarta fina de poma<br>amb gelat de vainilla i pinyons</li>
+                    <li>Figues confites au whisky et glace<br> ou <br>Tarte aux pommes, boule de glace<br>à la vanille et pignons de pins</li>
                     —<br>
                   </ul>
                 </div>
                 <div class="preu">
                   <p>32 €</p>
-                  <span>per persona</span>
+                  <span>par personne</span>
                 </div>
                 <!-- FIN MENU > TIPO MENU 1 -->
 
                 <!-- START MENU > TIPO MENU 2 -->
                 <div class="menu">
-                  <h4>Menú júnior</h4>
+                  <h4>Menu enfant</h4>
                   <ul>
-                    <li>Aperitiu Andorra a Taula</li>
+                    <li>Amuse-gueule Andorra a Taula</li>
                     —<br>
-                    <li>Croquetons de jarret de vedella<br>amb tomàquet amanit</li>
+                    <li>Grosses croquettes de jarret de veau<br>et tomate assaisonnée</li>
                     —<br>
-                    <li>Costelles de xai<br>amb patates fregides casolanes</li>
+                    <li>Côtes d’agneau<br>et pommes frites maison</li>
                     —<br>
-                    <li>Gelats artesans</li>
+                    <li>Glaces artisanales</li>
                   </ul>
                 </div>
                 <div class="preu">
                   <p>16 €</p>
-                  <span>per persona</span>
+                  <span>par personne</span>
                 </div>
                 <!-- FIN MENU > TIPO MENU 2 -->
             </div>
@@ -84,17 +84,17 @@
           <!-- START LEGAL + DIRECCIÓN -->
           <div class="info-restaurant">
             <div class="direccion">
-              <p>IGI inclòs • Begudes no incloses</p>
+              <p>TTC • Boissons non comprises</p>
             </div>
              <div class="boton">
-              <a href="https://www.hotelcoma.com" target="_blank">Fes la teva <br>reserva</a>
+              <a href="https://www.hotelcoma.com" target="_blank">Faites votre<br>réservation</a>
               <!-- SI ES UN MAIL PONER EN HREF MAILTO, EJEMPLO: <a href="mailto:xxxxxx@mail.com">Fes la teva <br>reserva</a>-->
             </div>
             <div class="direccion">
               <p>Camp de La Tanada, s/n · Ordino</p>
               <p>(+376) 736 100</p>
-              <p> De dilluns a diumenge, de 13 h a 15 h</p>
-              <p>i de 20 h a 22 h</p>
+              <p>Du lundi au dimanche, de 13 h à 15 h</p>
+              <p>et de 20 h à 22 h</p>
               <p>hotelcoma@hotelcoma.com</p>
               <br>
               <p><a href="https://www.hotelcoma.com/">www.hotelcoma.com</a></p>
