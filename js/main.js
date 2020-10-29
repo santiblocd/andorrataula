aa851e2e8627bdb2a3876eef1b0fe1db9217b25e
santiblocd <santi@blocd.com>
2020-10-29 09:33:31 +0100

    proximamente pastilla y safari fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/**
+ * If browser back button was used, flush cache
+ * This ensures that user will always see an accurate, up-to-date view based on their state
+ * https://stackoverflow.com/questions/8788802/prevent-safari-loading-from-cache-when-back-button-is-clicked
+ */
+(function () {
+  window.onpageshow = function(event) {
+    if (event.persisted) {
+      window.location.reload();
+    }
+  };
+})();
+
 $(window).load(function() {
     $("body").addClass("loaded");
     $('.circle.date').addClass('animation');
